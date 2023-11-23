0f26bc101c5aad0b373bd30dcf177434d19bf0c5
Alejandro A. Stefan-Zavala <aastefan@caltech.edu>
2023-11-22 17:33:19 -0800

    Checkpoint

--- config.h
+++ config.h
@@ -81,7 +81,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb",
-	col_gray1, "-nf", col_gray3, "-sb", col_maroon, "-sf", col_gray4, NULL };
+	col_gray1, "-nf", col_gray3, "-sb", col_gold, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *slockcmd[] = {"slock",NULL};
 
